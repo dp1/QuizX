c3103e9a0767356d7ae101cfa1acfc94ec3b8b9d
Dario Petrillo <dario.pk1@gmail.com>
2022-12-05 15:59:27 +0100

    Complete ShakeListener

--- app/app/src/main/java/com/labmacc/quizx/RankingActivity.kt
+++ app/app/src/main/java/com/labmacc/quizx/RankingActivity.kt
@@ -1,6 +1,7 @@
 package com.labmacc.quizx
 
 import android.content.Context
+import android.content.Intent
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -8,22 +9,77 @@ import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
 import com.labmacc.quizx.databinding.ActivityRankingBinding
 import com.labmacc.quizx.databinding.RankingUserBinding
 import kotlin.math.sqrt
 
-class ShakeListener : SensorEventListener {
+class ShakeListener(val triggerDelayMs: Long, val onTrigger: () -> Unit) : SensorEventListener {
+    companion object { const val TAG = "SHAKE" }
+
     private var filteredAcceleration = 0f
     private val alpha = 0.1f
+    private var gravity: FloatArray? = null
+
+    private val threshold = 3
+    private var lastTrigger: Long = 0
+
+    fun register(sensorManager: SensorManager) {
+        sensorManager.registerListener(
+            this,
+            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
+        sensorManager.registerListener(
+            this,
+            sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY),
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
+    }
+
+    fun unregister(sensorManager: SensorManager) {
+        sensorManager.unregisterListener(
+            this,
+            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        )
+        sensorManager.unregisterListener(
+            this,
+            sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY)
+        )
+
+        gravity = null
+    }
 
     override fun onSensorChanged(event: SensorEvent) {
-        val (x, y, z) = event.values
-        val acceleration = sqrt(x * x + y * y + z * z)
-        filteredAcceleration = filteredAcceleration * (1f - alpha) + acceleration * alpha
+        when (event.sensor.type) {
+            Sensor.TYPE_ACCELEROMETER -> {
+                if (gravity == null)
+                    return
 
-        Log.d("SHAKE", "accel $acceleration filteres $filteredAcceleration")
+                val x = event.values[0] - gravity!![0]
+                val y = event.values[1] - gravity!![1]
+                val z = event.values[2] - gravity!![2]
+
+                val acceleration = sqrt(x * x + y * y + z * z)
+                filteredAcceleration = filteredAcceleration * (1f - alpha) + acceleration * alpha
+
+                Log.d(TAG, "accel $acceleration filtered $filteredAcceleration")
+
+                if (filteredAcceleration >= threshold) {
+                    val currentTime = System.currentTimeMillis()
+                    if (currentTime - lastTrigger >= triggerDelayMs) {
+                        Log.i(TAG, "Triggered, filtered $filteredAcceleration")
+                        lastTrigger = currentTime
+                        onTrigger()
+                    }
+                }
+            }
+            Sensor.TYPE_GRAVITY -> {
+                gravity = event.values.clone()
+            }
+        }
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
@@ -35,8 +91,10 @@ class RankingActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRankingBinding
     private val vm: RankingViewModel by viewModels { RankingViewModel.Factory }
 
-    private val shakeListener = ShakeListener()
     private lateinit var sensorManager: SensorManager
+    private val shakeListener = ShakeListener(5_000) {
+        Toast.makeText(this, "Triggered", Toast.LENGTH_SHORT).show()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -64,18 +122,11 @@ class RankingActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
-        sensorManager.registerListener(
-            shakeListener,
-            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-            SensorManager.SENSOR_DELAY_NORMAL
-        )
+        shakeListener.register(sensorManager)
     }
 
     override fun onPause() {
         super.onPause()
-        sensorManager.unregisterListener(
-            shakeListener,
-            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-        )
+        shakeListener.unregister(sensorManager)
     }
 }
