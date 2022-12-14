bb93db773d81ffc54f7dec3e280be4e14200be8f
Dario Petrillo <dario.pk1@gmail.com>
2022-12-14 21:55:22 +0100

    Don't crash on null user

--- app/app/src/main/java/com/labmacc/quizx/data/CloudFirestoreDataSource.kt
+++ app/app/src/main/java/com/labmacc/quizx/data/CloudFirestoreDataSource.kt
@@ -27,8 +27,11 @@ class CloudFirestoreDataSource {
 
     suspend fun getUser(uuid: String): Result<User> {
         val res = db.collection("users").document(uuid).get().await()
-        val user = res.toObject<User>()!!
-        return Result.Success(user)
+        val user = res.toObject<User>()
+
+        return user?.let {
+            Result.Success(user)
+        } ?: Result.Error(IOException("Retrieved user is null"))
     }
 
     fun listenForRatingChanges(listener: (List<User>) -> Unit) {
