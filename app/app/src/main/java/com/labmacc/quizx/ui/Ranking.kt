3884ff04a7d4b799468b652c8a5c0db13daab7ed
Simone Gennenzi <simo694@gmail.com>
2022-12-14 11:46:29 +0100

    background color

--- app/app/src/main/java/com/labmacc/quizx/ui/Ranking.kt
+++ app/app/src/main/java/com/labmacc/quizx/ui/Ranking.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,7 +34,7 @@ fun Ranking(vm: RankingViewModel) {
     val (currentUser, _) = remember { vm.currentUser() }
 
     LazyColumn(modifier = Modifier
-        .background(Color.Cyan)
+        .background(colorResource(R.color.skyblue))
     ) {
         itemsIndexed(users, key = { _, user -> user.uuid }) { i, user ->
             RankingUser(i, user, currentUser)
