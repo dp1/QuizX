a0326df524bcb068734c605170fe6c623c29dd3c
Dario Petrillo <dario.pk1@gmail.com>
2022-12-26 13:29:10 +0100

    Submission logic

--- server/server.py
+++ server/server.py
@@ -4,7 +4,10 @@ import argparse
 
 import firebase_admin
 from firebase_admin import credentials, firestore
-from firebase_admin.firestore import ArrayUnion, Query
+from firebase_admin.firestore import ArrayUnion, ArrayRemove, Query, Increment
+
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
@@ -12,6 +15,35 @@ parser = argparse.ArgumentParser()
 parser.add_argument('credentials_file', help='Firebase credentials json file')
 args = parser.parse_args()
 
+"""
+"users": {
+    "u0": {
+        "uuid": "u0",
+        "displayName": "User Name",
+        "score": 42,
+        "pendingChallenges": ["q0", "q1"]
+    },
+    "u1": {
+        ...
+    }
+}
+
+"quizzes": {
+    "q0": {
+        "uuid": "q0",
+        "authorId": "u1",
+        "imageUri": "...",
+        "correctAnswer": "answer",
+        "sentToUsers": false,
+        "correctSubmissions": 3,
+        "wrongSubmissions": 2
+    }
+}
+"""
+
+USERS_ABOVE = 3
+USERS_BELOW = 3
+SCORE_K = 10
 
 cred = credentials.Certificate(args.credentials_file)
 firebase_admin.initialize_app(cred)
@@ -19,10 +51,6 @@ db = firestore.client()
 
 users_ref = db.collection('users')
 quizzes_ref = db.collection('quizzes')
-
-
-USERS_ABOVE = 3
-USERS_BELOW = 3
 
 def on_new_quiz(quiz_id, doc):
     doc = doc.to_dict()
@@ -72,6 +100,97 @@ def on_snapshot(doc_snapshot, changes, read_time):
 quizzes_ref.where('sentToUsers', '==', False).on_snapshot(on_snapshot)
 
 
-import time
-while True:
-    time.sleep(1000)
+app = Flask(__name__)
+api = Api(app)
+
+parser = reqparse.RequestParser()
+parser.add_argument('sender_id', required=True)
+parser.add_argument('quiz_id', required=True)
+parser.add_argument('answer', required=True)
+
+class QuizHub(Resource):
+    def answers_equal(self, a: str, b: str):
+        return a.lower().strip() == b.lower().strip()
+
+    def author_score(self, correct_subs: int, wrong_subs: int):
+        """
+        Returns a score in [-SCORE_K .. SCORE_K]
+        If every submission is correct, the author's score is -SCORE_K
+        If every submission is wrong, the score is SCORE_K
+        """
+        N = correct_subs + wrong_subs
+        if N == 0:
+            return 0
+        return (wrong_subs - correct_subs) * SCORE_K / N
+
+    def post(self):
+        args = parser.parse_args()
+        sender_id = args['sender_id']
+        quiz_id = args['quiz_id']
+        answer = args['answer']
+        logging.info(f'Received submission from {sender_id} on quiz {quiz_id} with answer {answer}')
+
+        sender = users_ref.document(sender_id).get()
+        if not sender.exists:
+            logging.warning(f'Sender {sender_id} not found. Skipping submisson')
+            return {"success": False}
+        sender = sender.to_dict()
+
+        quiz = quizzes_ref.document(quiz_id).get()
+        if not quiz.exists:
+            logging.warning(f'Quiz {quiz_id} not found. Skipping submission')
+            return {"success": False}
+        quiz = quiz.to_dict()
+        author_id = quiz['authorId']
+
+        author = users_ref.document(author_id).get()
+        if not author.exists:
+            logging.warning(f'Author {author_id} not found. Skipping submission')
+            return {"success": False}
+        author = author.to_dict()
+
+        correct_subs = quiz.get('correctSubmissions', 0)
+        wrong_subs = quiz.get('wrongSubmissions', 0)
+        author_score_before = self.author_score(correct_subs, wrong_subs)
+
+        correct = self.answers_equal(quiz['correctAnswer'], answer)
+        sender_score_delta = 0
+
+        if correct:
+            correct_subs += 1
+            sender_score_delta = SCORE_K
+        else:
+            wrong_subs += 1
+            sender_score_delta = -SCORE_K
+
+        author_score_after = self.author_score(correct_subs, wrong_subs)
+
+        logging.info(f'Updating scores: sender ({sender_id}) {sender_score_delta:+}, author ({author_id} {author_score_after - author_score_before:+}')
+
+        users_ref.document(sender_id).update({
+            'score': Increment(sender_score_delta),
+            'pendingChallenges': ArrayRemove([quiz_id])
+        })
+        users_ref.document(author_id).update({
+            'score': Increment(author_score_after - author_score_before)
+        })
+
+        if correct:
+            quizzes_ref.document(quiz_id).update({
+                'correctSubmissions': Increment(1)
+            })
+        else:
+            quizzes_ref.document(quiz_id).update({
+                'wrongSubmissions': Increment(1)
+            })
+
+        return {
+            "success": True,
+            "result": correct,
+            "score_obtained": sender_score_delta
+        }
+
+api.add_resource(QuizHub, '/submit')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0')
