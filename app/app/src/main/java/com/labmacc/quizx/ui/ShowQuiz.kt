45127eef3b199cfa0950709a339dfc5b88fb3da6
Dario Petrillo <dario.pk1@gmail.com>
2022-12-29 15:57:55 +0100

    More uniform styling

--- app/app/src/main/java/com/labmacc/quizx/ui/ShowQuiz.kt
+++ app/app/src/main/java/com/labmacc/quizx/ui/ShowQuiz.kt
@@ -3,15 +3,18 @@ package com.labmacc.quizx.ui
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -105,7 +108,10 @@ fun ShowQuiz(
                     .background(colorResource(R.color.skyblue))
             ) {
                 var scratchView by remember { mutableStateOf<ScratchView?>(null) }
-                AndroidView(modifier = Modifier.weight(1f),
+                AndroidView(modifier = Modifier
+                    .weight(1f)
+                    .padding(10.dp)
+                    .clip(RoundedCornerShape(10.dp)),
                     factory = { context -> ScratchView(context).apply { scratchView = this } },
                     update = { view ->
                         vm.image.value?.let {
