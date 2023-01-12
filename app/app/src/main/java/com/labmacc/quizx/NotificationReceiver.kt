f99c546443e87050689039b752b76bf5b31ba156
Dario Petrillo <dario.pk1@gmail.com>
2023-01-13 00:32:56 +0100

    Create notification from receiver

--- app/app/src/main/java/com/labmacc/quizx/NotificationReceiver.kt
+++ app/app/src/main/java/com/labmacc/quizx/NotificationReceiver.kt
@@ -1,12 +1,42 @@
 package com.labmacc.quizx
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationCompat
 
 class NotificationReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         Log.i("NotificationReceiver", "Triggered")
+
+        val channelId = "Notifications"
+        val channelName = "com.labmacc.quizx"
+
+        val target = Intent(context, RankingActivity::class.java)
+        target.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        val pendingIntent = PendingIntent.getActivity(context, 0, target, PendingIntent.FLAG_IMMUTABLE)
+        val builder = NotificationCompat.Builder(context, channelId)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+            .setContentTitle("New quiz")
+            .setContentText("You just received a new quiz to solve!")
+
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(0, builder.build())
+        Log.i(RankingActivity.TAG, "Notification sent!")
     }
 }
