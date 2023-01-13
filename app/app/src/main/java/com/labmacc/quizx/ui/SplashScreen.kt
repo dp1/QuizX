34eb2fb58c6683ef089903b3020793006f8d9159
Dario Petrillo <dario.pk1@gmail.com>
2023-01-13 15:12:14 +0100

    Add instructions to splash screen

--- app/app/src/main/java/com/labmacc/quizx/ui/SplashScreen.kt
+++ app/app/src/main/java/com/labmacc/quizx/ui/SplashScreen.kt
@@ -6,17 +6,23 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.labmacc.quizx.R
+import com.labmacc.quizx.ui.theme.wick
 import kotlinx.coroutines.delay
 
 @Composable
@@ -36,23 +42,41 @@ fun SplashScreen(
     LaunchedEffect(key1 = true) {
         scale.animateTo(
             targetValue = 0.7f,
-            animationSpec = tween(1000, easing = {
+            animationSpec = tween(1500, easing = {
                 OvershootInterpolator(4f).getInterpolation(it)
             })
         )
-        delay(1000)
+        delay(2000)
         onComplete()
     }
 
-    Image(
-        painter = painterResource(id = R.drawable.q1),
-        contentDescription = "",
-        alignment = Alignment.Center,
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(40.dp)
-            .scale(scale.value)
-    )
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.q1),
+                contentDescription = "",
+                alignment = Alignment.Center,
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth()
+                    .padding(40.dp)
+                    .scale(scale.value)
+            )
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = "Shake to create a quiz",
+                textAlign = TextAlign.Center,
+                color = Color.LightGray,
+                fontFamily = wick,
+                fontSize = 20.sp
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
